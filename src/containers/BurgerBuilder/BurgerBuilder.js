95ee93a83ff8dd42b38ca6408f6de38b8c1416dc
karlo <deguzmankarlo@gmail.com>
2020-03-23 17:02:48 +0000

    Add functionality to more ingredient button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,18 +7,29 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1, 
-      cheese: 2, 
-      meat: 2
+      salad: 0,
+      bacon: 0, 
+      cheese: 0, 
+      meat: 0
     }
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    this.setState({ingredients: updatedIngredients});
   }
 
   render () {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls 
+          ingredientAdded={this.addIngredientHandler}/>
       </Aux>
     );
   }
